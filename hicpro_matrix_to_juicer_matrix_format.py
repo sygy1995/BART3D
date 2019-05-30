bbed16347a4409191e548470d19be1dba821b364
sygy1995 <yz4hc@virginia.edu>
2019-05-30 16:05:49 -0400

    speed up file loading

--- hicpro_matrix_to_juicer_matrix_format.py
+++ hicpro_matrix_to_juicer_matrix_format.py
@@ -2,34 +2,85 @@ import sys,argparse
 import os,glob
 import numpy as np
 import pandas as pd
+import bisect
 
 import utils
 
 
-def write_out_juicer_format_matrix(order_index_file,matrix_file,outdir,data,resolution,flag):
+# def write_out_juicer_format_matrix(order_index_file,matrix_file,outdir,data,resolution,flag):
 
-    # read index file
+#     # read index file
+#     with open(order_index_file) as order_index_inf: 
+#         index_df = pd.read_csv(order_index_inf,sep='\t',header=None)
+#     index_df.columns = ['chr','x','y','id']
+    
+#     # split the original matrix by chroms
+#     for chrom in utils.chroms:
+#         outfile = open(outdir+os.sep+'{}_{}_{}_{}.matrix'.format(data,resolution,flag,chrom),'w')
+#         index_df_chr = index_df[index_df['chr']==chrom]
+#         index_df_chr.index = index_df_chr['id']
+#         index_dict = index_df_chr[['x']].to_dict()['x']
+#         keys = index_dict.keys()
+        
+#         matrix_inf = open(matrix_file)
+#         line = matrix_inf.readline()
+#         while line:
+#             sline = line.strip().split()
+#             id_a,id_b,score = int(sline[0]),int(sline[1]),float(sline[2])
+#             if (id_a in keys) and (id_b in keys):
+#                 outfile.write('{}\t{}\t{:.2f}\n'.format(index_dict[id_a],index_dict[id_b],score))
+#             line = matrix_inf.readline()
+#         matrix_inf.close()
+#         outfile.close()
+
+def write_out_juicer_format_matrix(order_index_file,matrix_file,outdir,data,resolution,flag):
+    # load in the index bed file
     with open(order_index_file) as order_index_inf: 
         index_df = pd.read_csv(order_index_inf,sep='\t',header=None)
     index_df.columns = ['chr','x','y','id']
-    
-    # split the original matrix by chroms
+
+    # seperate index for each chromosome into distinct dataframes
+    chrome_index_range = {}
+    for chrom in utils.chroms:
+        vars()['index_df_'+chrom] = index_df[index_df['chr']==chrom]
+        # save the start and end index of each chromosome into a dict
+        chrome_index_range[chrom] = [min(vars()['index_df_'+chrom]['id']),max(vars()['index_df_'+chrom]['id'])]
+
+    # load in the matrix file
+    matrix_df = pd.read_csv(matrix_file,sep='\t',header=None)
+    matrix_df.columns = ['id1','id2','count']
+    for chrom in utils.chroms:
+        # assume that the matrix file is already sorted
+        # bisect out the first and last id1 that is in this chromosome
+        low1 = bisect.bisect_left(matrix_df['id1'],chrome_index_range[chrom][0])
+        high1 = bisect.bisect_right(matrix_df['id1'],chrome_index_range[chrom][1])
+        # save all interaction starts with this chromosome into a distinct dataframe
+        vars()['matrix_df_'+chrom] = matrix_df[low1:high1]
+        # sort this chromosomal specific interaction matrix by id2
+        vars()['matrix_df_'+chrom] = vars()['matrix_df_'+chrom].sort_values(['id2'])
+        # reassign the index to be from 0 to length
+        vars()['matrix_df_'+chrom] = vars()['matrix_df_'+chrom].set_index(pd.Index(list(range(0,len(vars()['matrix_df_'+chrom])))))
+        # bisect out the first and last id2 that is in this chromosome
+        low2 = bisect.bisect_left(vars()['matrix_df_'+chrom]['id2'],chrome_index_range[chrom][0])
+        high2 = bisect.bisect_right(vars()['matrix_df_'+chrom]['id2'],chrome_index_range[chrom][1])
+        # update this chromosomal specific interaction matrix with interaction that both id1 and id2 are in this chromosome
+        vars()['matrix_df_'+chrom] = vars()['matrix_df_'+chrom][low2:high2]
+        # sort this chromosomal specific interaction matrix by id1 and then id2
+        vars()['matrix_df_'+chrom] = vars()['matrix_df_'+chrom].sort_values(by=['id1','id2'])
+        # reassign the index to be from 0 to length
+        vars()['matrix_df_'+chrom] = vars()['matrix_df_'+chrom].set_index(pd.Index(list(range(0,len(vars()['matrix_df_'+chrom])))))
+
+    #remove the raw matrix dataframe from variables to save memory
+    del matrix_df
+
+    # basically the same idea as previouse, just that we are only loading chromosomal specific interaction matrix in each iteration
     for chrom in utils.chroms:
         outfile = open(outdir+os.sep+'{}_{}_{}_{}.matrix'.format(data,resolution,flag,chrom),'w')
-        index_df_chr = index_df[index_df['chr']==chrom]
-        index_df_chr.index = index_df_chr['id']
-        index_dict = index_df_chr[['x']].to_dict()['x']
-        keys = index_dict.keys()
-        
-        matrix_inf = open(matrix_file)
-        line = matrix_inf.readline()
-        while line:
-            sline = line.strip().split()
-            id_a,id_b,score = int(sline[0]),int(sline[1]),float(sline[2])
-            if (id_a in keys) and (id_b in keys):
-                outfile.write('{}\t{}\t{:.2f}\n'.format(index_dict[id_a],index_dict[id_b],score))
-            line = matrix_inf.readline()
-        matrix_inf.close()
+        vars()['index_df_'+chrom].index = vars()['index_df_'+chrom]['id']
+        index_dict = vars()['index_df_'+chrom][['x']].to_dict()['x']
+        for line in list(zip(vars()['matrix_df_'+chrom]['id1'],vars()['matrix_df_'+chrom]['id2'],vars()['matrix_df_'+chrom]['count'])):
+            id_a,id_b,score = int(line[0]),int(line[1]),float(line[2])
+            outfile.write('{}\t{}\t{:.2f}\n'.format(index_dict[id_a],index_dict[id_b],score))
         outfile.close()
 
 def main(data,resolution):
