998bc4f14670e87e11a905f0973d246bfe932315
sygy1995 <yz4hc@virginia.edu>
2019-08-14 11:01:17 -0400

    remove some test variables

--- get_interaction_change.py
+++ get_interaction_change.py
@@ -9,11 +9,9 @@ def compare_hic_interaction(control_np,treatment_np,resolution,file,chrom,specie
 	compr_data_out = open(file,'a')
 
 	bin_number = len(control_np)
-	test = 0
 	for i in range(bin_number):
 	    stats_score,pvalue = stats.ttest_rel(treatment_np[i],control_np[i])
 	    if np.isnan(pvalue):
 	        pvalue = 1
-	        test = test + 1
 	    start = i*resolution
 	    compr_data_out.write('{}\t{}\t{}\t.\t{:.3f}\t.\n'.format(chrom, start, start+resolution, -np.log10(pvalue)))
